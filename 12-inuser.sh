7c9b1d0358947bc7e95332291ee834b004226097
Raoni Normanton <raonifn@gmail.com>
2020-02-17 14:15:16 -0300

    fix

--- 12-inuser.sh
+++ 12-inuser.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- 13-i3.sh
+++ 13-i3.sh
@@ -2,10 +2,6 @@
 
 sudo pacman -S --noconfirm i3-wm xorg-server xorg xf86-video-intel mesa xorg-xinit dmenu feh scrot termite i3lock i3status pavucontrol arandr pulseaudio pulseaudio-alsa awesome-terminal-fonts ttf-font-awesome dunst libnotify network-manager-applet
 
-cat <<END > $HOME/.xinitrc
-systemctl --user import-environment DISPLAY
-exec i3
-END
 cat <<END | sudo tee /etc/X11/xorg.conf.d/00-keyboard.conf
 Section "InputClass"
  	Identifier "system-keyboard"
@@ -27,3 +23,7 @@ systemctl --user start pulseaudio.socket
 
 cd $HOME
 git clone https://github.com/raonifn/my-i3.git .i3
+
+git clone https://github.com/raonifn/dotfiles .dotfiles
+./.dotfiles/install.sh
+
